ec8b57c3350c1168babc36be4ab28fba3dc90556
MoyHacks <moysainzl@gmail.com>
2022-08-30 17:45:43 -0600

    se agregan select de generos

---
+++ public/src/js/genre.js
@@ -0,0 +1,72 @@
+const API_KEY = "f28711faa6dd23ead57c3434bcbea433";
+const imageUrl="https://image.tmdb.org/t/p/w220_and_h330_face";
+const URL_IMAGES = "https://image.tmdb.org/t/p";
+const videoUrl="https://www.youtube.com/watch?v="
+const detailUrl= "/detalle.html?id=";
+
+async function getMoviesByGenre(id){
+    let moviesUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=${id}`;
+    try {
+        let response = await axios.get(moviesUrl);
+        console.log(response.data.results);
+        return response.data.results
+    } catch (e) {
+        return []
+    }
+}
+
+async function getGenreMovies() {
+    let moviesUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`;
+    try {
+        let response = await axios.get(moviesUrl);
+        return response.data;
+    } catch (e) {
+        return []
+    }
+}
+
+async function screenGenre(){
+   
+    let divGenre = document.getElementById("genre");
+    const urlParams = new URLSearchParams(window.location.search);
+    idGenre = urlParams.get('id');
+    let selectInput = document.getElementById('genres');
+    selectInput.value=idGenre;
+    let movies = await getMoviesByGenre(idGenre);
+    console.log(movies);
+      for(movie of movies) {
+         if(movie.poster_path) {
+            let card =  document.createElement('div');
+             card.classList = "card mb-2 poster-pelicula";
+             card.setAttribute("id", movie.id);
+             card.addEventListener("click", e => {
+                window.location = `detalle.html?id=${movie.id}`;
+            });
+            card.innerHTML = `
+                    <img src="${URL_IMAGES}/w500/${movie.poster_path}" alt="${movie.title}" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">${movie.title}</h5>
+                    </div>
+                `;
+                divGenre.appendChild(card);   
+         }
+     };
+     if(!Number(idGenre)) {
+        window.location = "principal.html";
+    }
+       
+ };
+
+ 
+ getGenreMovies().then((movies)=>{
+    let select=document.getElementById('genres');
+    for(let movie of movies.genres){
+    var option = new Option(movie.name,movie.id);
+       select.appendChild(option);
+    };
+    console.log(movies.genres);
+});
+
+
+
+ screenGenre();
--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,19 +1,23 @@
 
-let API_KEY = "f28711faa6dd23ead57c3434bcbea433";
-let imageUrl="https://image.tmdb.org/t/p/w220_and_h330_face";
+//let actor="https://api.themoviedb.org/3/search/person?api_key=9eb4e5199ac559b2a5a63d0b43ea5c76&language=en-US&query="+actor+"&page=1&include_adult=false";
 
+const API_KEY = "f28711faa6dd23ead57c3434bcbea433";
+const imageUrl="https://image.tmdb.org/t/p/w220_and_h330_face";
+const URL_IMAGES = "https://image.tmdb.org/t/p";
+const videoUrl="https://www.youtube.com/watch?v="
+const detailUrl= "/detalle.html?id=";
 
     async function getMovie(id) {
-    let url =`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`;
+    let url =`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=en-US`;
     try {
-        let response = await axios.get(url)
-        return response.data
+        let response = await axios.get(url);
+
+        return response.data;
     } catch (e) {
         console.log(e)
         return null;
     }
     }
-
 
     async function getLatestMovies() {
         let moviesUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`;
@@ -35,36 +39,102 @@ let imageUrl="https://image.tmdb.org/t/p/w220_and_h330_face";
         }
     }
 
-
-    function setImage(id,url,div,type){
-        const categoria=document.getElementById(div);
-        const image = document.createElement('img');
-            image.setAttribute('id',id);
-            image.setAttribute('src',url);
-            image.setAttribute('class',type);
-            categoria.appendChild(image);
+    async function getGenreMovies() {
+        let moviesUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`;
+        try {
+            let response = await axios.get(moviesUrl);
+            return response.data;
+        } catch (e) {
+            return []
+        }
     }
 
 
-    getRatedMovies().then((movies) => {
+
+    async function setImage(id,url,div,type,bool){
+        const category=document.getElementById(div);
+        const image = document.createElement('img');
+        const href = createAHref(id,bool);
+        const span = await createSpan(id);
+            image.setAttribute('id',id);
+            image.setAttribute('src',url);
+            image.setAttribute('class',type);
+            category.appendChild(href).appendChild(image);
+    }
+
+
+
+ 
+
+     function createAHref(id,bool){ 
+        const link = document.createElement('a');
+        if(!bool){
+            link.setAttribute('href','#');
+            return link;
+        }
+        link.setAttribute('target','__self');
+        link.setAttribute('href',detailUrl+id);
+        return link;
+        
+        
+    }
+
+    async function createSpan(id){ 
+        const span = document.createElement('span');
+           let movie ='#';
+       /* let movie = await getMovie(id).then((m)=>{      
+                return videoUrl+m.results[0].key?'':'#';          
+        });*/
+        span.setAttribute('target','__self');
+        span.setAttribute('href',movie);
+        return span;
+    }
+
+
+
+    
+  getRatedMovies().then((movies) => {
         let i = 0;
         for (let movie of movies) {
-            console.log(movie)
+            console.log(movie);
+            consturl2=URL_IMAGES+"/w500/"+movie.poster_path;
             const url= imageUrl+movie.poster_path;
             console.log(url);
             if(i<10){
-                setImage(i++,"public/src/images/top10/"+i+".png",'top_10','top10');
-                setImage(movie.id,url,'top_10','row__poster top10');
-            }
-            
+                setImage(i++,"public/src/images/top10/"+i+".png",'top_10','top10',false);
+                setImage(movie.id,url,'top_10','row__poster top10',true);
+               
+            }     
         }
     });
 
-    getLatestMovies().then((movies) => {
+
+
+     getLatestMovies().then((movies) => {
+        let i=0;
         for (let movie of movies) {
+            if (i<15){
             console.log(movie)
             const url= imageUrl+movie.poster_path;
             console.log(url);
-            setImage(movie.id,url,'top_rated','row__poster');
+            setImage(movie.id,url,'top_rated','row__poster',true);
+            i++;
+        }
         }
     });
+
+     getGenreMovies().then((movies)=>{
+        let select=document.getElementById('genres');
+        for(let movie of movies.genres){
+        var option = new Option(movie.name,movie.id);
+           select.appendChild(option);
+        };
+        console.log(movies.genres);
+    });
+
+
+    
+   
+
+
+    
